204ea6e32951657a6f53f80f5ea8802aa0c6b0b1
Dai Bui <shindomax@gmail.com>
2025-10-13 09:10:49 +0700

    fix bottomtab

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -466,7 +466,7 @@ function MapPageContent() {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 safe-bottom">
+    <div className="min-h-screen bg-gray-50 safe-bottom with-bottom-tab">
       {/* Professional Header - Single Row Layout */}
       <div className="bg-white border-b border-gray-200 shadow-sm">
         <div className="px-4 py-3">
@@ -733,7 +733,7 @@ function MapPageContent() {
       </div>
 
       {/* Map and Detail Layout */}
-      <div className="flex flex-col lg:flex-row h-[calc(100vh-180px)] lg:h-[calc(100vh-180px)] pb-16 lg:pb-0">
+      <div className="flex flex-col lg:flex-row h-[calc(100vh-180px)] lg:h-[calc(100vh-180px)]">
         <div className="flex-1 order-2 lg:order-1 relative">
           {loading ? (
             <div className="h-full flex items-center justify-center">
